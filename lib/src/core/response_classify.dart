0c4d440a72d209fb7aaecbef561e7ecad857fbfc
@DIPazulhardiyanto <dip.achmadz@gmail.com>
2022-10-15 19:37:47 +0700

    added initial commit

---
+++ lib/src/core/response_classify.dart
@@ -0,0 +1,18 @@
+class ResponseClassify<T> {
+  Status status;
+  T? data;
+  String? error;
+
+  ResponseClassify.loading() : status = Status.loading;
+
+  ResponseClassify.completed(this.data) : status = Status.completed;
+
+  ResponseClassify.error(this.error) : status = Status.error;
+
+  @override
+  String toString() {
+    return "Status : $status \n Message :  \n Data : $data error : $error";
+  }
+}
+
+enum Status { loading, completed, error }
